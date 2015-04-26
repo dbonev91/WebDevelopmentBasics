df05509847a2979e0d7886229315c3f759025c6b
Dimitar Bonev <dbonev91@users.noreply.github.com>
2015-04-26 19:46:12 +0300

    gatakka framework MVC. (App, Loader, Config)

---
+++ gf/App.php
@@ -0,0 +1,50 @@
+<?php
+namespace GF;
+include_once 'Loader.php';
+
+class App {
+        private static $_instance = null;
+        private $_config = null;
+    
+        private function __construct () {
+                $namespace = "GF";
+                $path = dirname(__FILE__) . DIRECTORY_SEPARATOR;
+                
+                \GF\Loader::registerNamespace($namespace, $path);
+                \GF\Loader::registerAutoload();
+                $this->_config = \GF\Config::getInstance();
+                
+                if ($this->_config->getConfigFolder() == null) {
+                        $this->_config->setConfigFolder('../config');
+                }
+        }
+    
+        public function setConfigFolder ($path) {
+                $this->_config->setConfigFolder($path);
+        }
+    
+        public function getConfigFolder () {
+                return $this->_config->_configFolder;
+        }
+    
+        /*
+                @return type: \GF\Config
+        */
+        public function getConfig () {
+                return $this->_config;
+        }
+    
+        public function run () {
+                if ($this->_config->getConfigFolder() == null) {
+                        $this->_config->_setConfigFolder('../config');
+                }
+        }
+    
+        public static function getInstance () {
+                if (self::$_instance == null) {
+                       self:: $_instance = new \GF\App();
+                }
+            
+                return self::$_instance;
+        }
+}
---
+++ gf/Loader.php
@@ -0,0 +1,75 @@
+<?php
+namespace GF;
+
+final class Loader {
+        private static $namespaces = array();
+    
+        private function __construct () {
+        }
+    
+        public static function registerAutoload () {
+                $autoloadObj = array("\GF\Loader", "autoload");
+                spl_autoload_register($autoloadObj);
+        }
+    
+        public static function autoload ($class) {
+                self::loadClass($class);
+        }
+    
+        public static function loadClass ($class) {
+                foreach (self::$namespaces as $key => $value) {
+                        if (strpos($class, $key) === 0) {
+                                $file = substr_replace($class, $value, 0, strlen($key)) . '.php';
+                                $isRealPath = realpath($file);
+                                
+                                if ($isRealPath && is_readable($isRealPath)) {
+                                        include $file;
+                                }
+                                else {
+                                        // TODO
+                                        throw new invalidargumentexception("File cannot be included: " . $file);
+                                }
+                            
+                                break;
+                        }
+                }
+        }
+    
+        public static function registerNamespace ($namespace, $path) {
+                $namespace = trim($namespace);
+                
+                if (strlen($namespace) > 0) {
+                        if ($path) {
+                                $_path = realpath($path);
+                                $isRealPath = $_path && is_dir($_path) && is_readable($_path);
+
+                                if ($isRealPath) {
+                                        self::$namespaces[$namespace . DIRECTORY_SEPARATOR] = $_path . DIRECTORY_SEPARATOR;
+                                }
+                                else {
+                                        // TODO
+                                        throw new invalidargumentexception("Namespace dir read error: " . $path);
+                                }   
+                        }
+                        else {
+                                throw new invalidargumentexception("Invalid path: " . $path);
+                        }
+                }
+                else {
+                        // TODO: main catching and do somethings with errors
+                        throw new invalidargumentexception("Invalid namespace: " . $namespace);
+                }
+        }
+    
+        public static function getNamespaces () {
+                return self::$namespaces;
+        }
+    
+        public static function removeNamespaces ($namespace) {
+                unset(self::$namespaces[$namespace]);
+        }
+    
+        public static function clearNamespaces () {
+                self::$namespaces = array();
+        }
+}
